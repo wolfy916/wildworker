077a249b555caf8f4951df706c202f4387a5ea66
권태형 <power916@daum.net>
2023-03-13 15:31:52 +0900

    kth: Feat: 인트로 페이지 구축, App.js 화면 고정

--- FE/front/src/pages/MainPage.js
+++ FE/front/src/pages/MainPage.js
@@ -1,20 +1,14 @@
 import * as React from "react"
-import Box from "@mui/material/Box"
-import Container from "@mui/material/Container"
 import "./MainPage.css"
 import character from "../asset/image/moving_man.gif"
 
 function MainPage() {
   return (
-    <Container className="container" maxWidth="xs">
-      <Box className="bg" sx={{ height: "100vh" }}>
-        <div className="subway-background">
-          <div className="subway">
-            <img src={character} alt="character" />
-          </div>
-        </div>
-      </Box>
-    </Container>
+    <div className="subway-background">
+      <div className="subway">
+        <img src={character} alt="character" />
+      </div>
+    </div>
   )
 }
 
