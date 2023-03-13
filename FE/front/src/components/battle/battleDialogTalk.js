ba9836cb0157736fcd4f03028d184dd7a9081cdd
wonseokjee <wsji9404@gmail.com>
2023-03-13 09:20:38 +0900

    jws: feat: pvp페이지 main 캐릭터 속도 조절

---
+++ FE/front/src/components/battle/battleDialogTalk.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import "./battleDialogTalk.css";
+
+export default function BattleDialogTalk() {
+  return (
+    <Box className="battleDialogTalk">
+      <p>뭐 임마</p>
+    </Box>
+  );
+}
