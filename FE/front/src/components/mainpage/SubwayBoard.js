52da29a6f8f4979d0582330d513115d98187d2c1
권태형 <power916@daum.net>
2023-03-14 14:15:01 +0900

    kth: Feat: 메인페이지 전광판 프로토타입 완료

--- FE/front/src/components/mainpage/SubwayBoard.js
+++ FE/front/src/components/mainpage/SubwayBoard.js
@@ -5,6 +5,7 @@ function SubwayBoard() {
   const NICKNAME = "우주최강원석";
   const BADGE = "사당역의 지배자";
   const COIN = 1500;
+  const COIN_COMMA = COIN.toLocaleString('ko-KR');
   const CURRENT_STATION = "역삼역";
   const CURRENT_STATION_DOMINATOR = "매의호크민성";
 
@@ -22,26 +23,22 @@ function SubwayBoard() {
             <span className="board-nickname">{NICKNAME}</span>
           </div>
           <div className="board-part second-part">
-            <span className="board-coin">{COIN} 원</span>
+            <span className="board-coin">남은 잔액 : {COIN_COMMA} 원</span>
           </div>
           <div className="board-part third-part">
-            <div>
-              이번 역은
-              <span className="current-station"> {CURRENT_STATION} </span>
-              입니다.
-            </div>
-            <div>
-              <span className="current-station">
-                {" "}
-                {CURRENT_STATION}의 지배자{" "}
-              </span>
-              는
-              <span className="current-station">
-                {" "}
-                {CURRENT_STATION_DOMINATOR}{" "}
-              </span>
-              입니다.
-            </div>
+            이번 역은
+            <span className="current-station"> {CURRENT_STATION} </span>
+            입니다.
+            <span className="current-station">
+              {" "}
+              {CURRENT_STATION}의 지배자{" "}
+            </span>
+            는
+            <span className="current-station">
+              {" "}
+              {CURRENT_STATION_DOMINATOR}{" "}
+            </span>
+            입니다.
           </div>
         </div>
       </div>
--- FE/front/src/pages/MainPage.js
+++ FE/front/src/pages/MainPage.js
@@ -5,14 +5,15 @@ import goMap from "../asset/image/goMap.png"
 import { Link } from "react-router-dom"
 import SubwayBoard from "../components/mainpage/SubwayBoard";
 
+
+
 function MainPage() {
   return (
     <div className="subway-background">
       <SubwayBoard/>
       <div className="subway">
         <img src={character} alt="character" />
-        <Link to="/map">
-          {" "}
+        <Link className="router-map-btn" to="/map">
           <img src={goMap} alt="goMap" />
         </Link>
       </div>
