351bd747b9b6f5be65b805d0c1b9ca09b5ae2d37
Seyoung Goo <0917sy@gmail.com>
2023-03-14 17:45:23 +0900

    feat: add websocket config

---
+++ BE/wild-worker/src/main/java/com/a304/wildworker/websocket/config/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.a304.wildworker.websocket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${allowed-origins}")
+    private final String[] allowedOrigins;
+
+//    private final ChannelInterceptor interceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins)
+                .withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/sub", "/queue");
+        config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(interceptor);
+    }
+}
