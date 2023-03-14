eb73859097509e1f772e45c6fdb92e6efada7597
sangjun <tkdwns9494@gmail.com>
2023-03-14 14:58:21 +0900

    ksj: Feat: 노선도 작업

--- FE/front/src/pages/MainPage.js
+++ FE/front/src/pages/MainPage.js
@@ -1,16 +1,17 @@
 import * as React from "react"
+import { useState } from "react"
 import "./MainPage.css"
 import character from "../asset/image/moving_man.gif"
 import goMap from "../asset/image/goMap.png"
 import { Link } from "react-router-dom"
 
 function MainPage() {
+  
   return (
     <div className="subway-background">
       <div className="subway">
         <img src={character} alt="character" />
         <Link to="/map">
-          {" "}
           <img src={goMap} alt="goMap" />
         </Link>
       </div>
