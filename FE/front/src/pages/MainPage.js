23bdd867341e011fc2ebd3aed430cf24ca393905
권태형 <power916@daum.net>
2023-03-14 17:53:34 +0900

    Feat: 메인 페이지 UI 구성

--- FE/front/src/pages/MainPage.js
+++ FE/front/src/pages/MainPage.js
@@ -2,9 +2,10 @@ import * as React from "react"
 import "./MainPage.css"
 import character from "../asset/image/moving_man.gif"
 import goMap from "../asset/image/goMap.png"
+import menuBtn from "../asset/image/mainpage_menu_btn.png";
+import getCoinBtn from "../asset/image/get_coin_btn.png"
 import { Link } from "react-router-dom"
 import SubwayBoard from "../components/mainpage/SubwayBoard";
-
 
 
 function MainPage() {
@@ -12,7 +13,9 @@ function MainPage() {
     <div className="subway-background">
       <SubwayBoard/>
       <div className="subway">
-        <img src={character} alt="character" />
+        <img className="character" src={character} alt="character" />
+        <img className="main-menu-btn" src={menuBtn} alt="menuBtn" />
+        <img className="get-coin-btn" src={getCoinBtn} alt="getCoinBtn" />
         <Link className="router-map-btn" to="/map">
           <img src={goMap} alt="goMap" />
         </Link>
