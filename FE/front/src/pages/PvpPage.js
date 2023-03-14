0933ca8e43dad171ebd3e98370e6534b7364e6af
wonseokjee <wsji9404@gmail.com>
2023-03-14 16:32:44 +0900

    jws: Refactor: Pvp페이지 오류 수정, container 벗겨냄

--- FE/front/src/pages/PvpPage.js
+++ FE/front/src/pages/PvpPage.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import BattleDialog from "../components/battle/battleDialog";
 import BattleCharater from "../components/battle/battlecharacter";
 import battleDirection from "../asset/image/battleDirection.png";
@@ -9,27 +8,25 @@ import "./PvpPage.css";
 
 function MainPage() {
   return (
-    <Container className="container" maxWidth="xs">
-      <Box className="PvpPageBg" sx={{ position: "relative" }}>
-        {/* <img
+    <Box className="PvpPageBg" sx={{ position: "relative" }}>
+      {/* <img
           className="pvpPageLoading"
           src={pvpPageLoading}
           alt="pvpPageLoading"
         /> */}
-        <div className="battleCharacter1">
-          <img src={battleDirection} alt="battleDirection" />
-          <BattleCharater />
-          <p>신도림의 지배자 권태형</p>
-        </div>
-        <div className="battleCharacter2">
-          <img src={battleDirection} alt="battleDirection" />
-          <BattleCharater className="char" />
-          <p>신도림의 지배자 권태형</p>
-        </div>
+      <div className="battleCharacter1">
+        <img src={battleDirection} alt="battleDirection" />
+        <BattleCharater />
+        <p>신도림의 지배자 권태형</p>
+      </div>
+      <div className="battleCharacter2">
+        <img src={battleDirection} alt="battleDirection" />
+        <BattleCharater className="char" />
+        <p>신도림의 지배자 권태형</p>
+      </div>
 
-        <BattleDialog />
-      </Box>
-    </Container>
+      <BattleDialog />
+    </Box>
   );
 }
 
