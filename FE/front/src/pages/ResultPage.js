f1e0b4ef705f15cecf53f9423ad0522a00c67e11
wonseokjee <wsji9404@gmail.com>
2023-03-15 10:30:38 +0900

    feat: 결과페이지 UI완성, 결과 영수증페이지 생성

--- FE/front/src/pages/ResultPage.js
+++ FE/front/src/pages/ResultPage.js
@@ -1,23 +1,35 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import BattleDialog from "../components/battle/battleDialog";
 import BattleCharater from "../components/battle/battlecharacter";
+import battleDialogImg from "../asset/image/battleTalk.png";
 
-import "./PvpPage.css";
+import "./ResultPage.css";
 
 function MainPage() {
   return (
-    <Box className="PvpPageBg" sx={{ position: "relative" }}>
-      <div className="battleCharacter1">
-        <BattleCharater />
-        <p>신도림의 지배자 권태형</p>
-      </div>
-      <div className="battleCharacter2">
-        <BattleCharater />
-        <p>신도림의 지배자 권태형</p>
+    <Box className="Pvp-Result" sx={{ position: "relative" }}>
+      <div className="battle-result-1">
+        <p className="battle-result-p1">신도림의 지배자 권태형</p>
+        <div className="battle-result-char">
+          <BattleCharater />
+        </div>
+        <p className="battle-result-p2">승리</p>
+        <p className="battle-result-p3">맞춘 개수: 12개</p>
       </div>
 
-      <BattleDialog />
+      <div className="battle-result-2">
+        <p className="battle-result2-p1">신도림의 지배자 권태형</p>
+        <div className="battle-result2-char">
+          <BattleCharater />
+        </div>
+        <p className="battle-result2-p2">패배</p>
+        <p className="battle-result2-p3">맞춘 개수: 10개</p>
+      </div>
+
+      <div className="battleResult-talk">
+        <img src={battleDialogImg} alt="battleDialogImg" />
+        <p className="battleResult-talk-p">결투에서 승리했다!!!!</p>
+      </div>
     </Box>
   );
 }
