1f79f31ec165e058ce97a9af114b332c5e3e767c
오민성 <qww1552@naver.com>
2023-03-16 17:18:59 +0900

    feat: station contract 구현

--- BC/contracts/Station.sol
+++ BC/contracts/Station.sol
@@ -4,10 +4,15 @@ pragma solidity 0.8.19;
 
 contract Station {
 
-    uint16 public stationId;
+    uint16 public id;
     string public name;
 
     mapping(address => uint) public investors;
+
+    constructor(uint16 _id, string memory _name) {
+        id = _id;
+        name = _name;
+    }
 
     // todo 지하철 진입 시 돈 지급
 
