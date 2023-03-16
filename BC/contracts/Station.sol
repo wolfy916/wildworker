5f8060561e0001e0da113978a3c0d2857f79f00d
오민성 <qww1552@naver.com>
2023-03-16 16:55:17 +0900

    feat: create station contract

---
+++ BC/contracts/Station.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract Station {
+
+    uint16 public stationId;
+    string public name;
+
+    mapping(address => uint) public investors;
+
+    // todo 지하철 진입 시 돈 지급
+
+    // todo 게임에 대한 승리 수당 지급
+
+    // todo 투자에 대한 처리
+    
+}
