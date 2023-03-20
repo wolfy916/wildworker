77d99d78c8ae9b00d6770d2f5f28c5b7623cb31a
Seyoung Goo <0917sy@gmail.com>
2023-03-20 17:05:12 +0900

    feat: add constants

---
+++ BE/wild-worker/src/main/java/com/a304/wildworker/common/Constants.java
@@ -0,0 +1,13 @@
+package com.a304.wildworker.common;
+
+public final class Constants {
+
+    // Session name
+    public static final String USER = "user";
+    public static final String ACCESS_TOKEN = "access_token";
+
+    public static final String URI = "localhost:8080/api/v1";
+
+    public static final Long noneTitle = 1L;
+
+}
