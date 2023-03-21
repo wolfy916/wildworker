f705d83a14a755a243d89b2e50ff825d4cfec56c
wonseokjee <wsji9404@gmail.com>
2023-03-21 15:04:06 +0900

    Feat: 수동채굴 기능 추가, transfer중복 부분 함수로 실행

--- BC/contracts/WonToken.sol
+++ BC/contracts/WonToken.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Won {
+contract WonToken {
     string public name = "Won Token";
     string public symbol = "Won";
-    uint256 public totalSupply = 10000000000; // 1 million tokens 100억개
+    // uint256 public totalSupply = 10000000000000000000000000000; // 1 million tokens 100억개
+    uint256 public totalSupply = 100000000000; // 1 million tokens 100억개
     uint8 public decimals = 0;
+    address owner;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     mapping(address => uint256) public balanceOf;
 
+    //밑줄 문자를 입력하면 함수가 계속되게 됨 위 제어자를 실행한 다음 함수로 이어지라는 것.
+    //조건에 부합하지 않는다면 함수를 계속하지 않아서 접근하는 것을 막음.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "onlyOwner can call this function");
+        _;
+    }
+
     constructor() {
         balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
     function transfer(
@@ -29,23 +39,13 @@ contract Won {
         return true;
     }
 
-    //수동채굴 ( owner가 발급하도록)
-
-    //게임비 유저로부터 지하철 역에 전달
-    function sendGameCost(
-        address _toStation,
-        uint256 _value
-    ) public payable returns (bool success) {
-        //user(msg.sender)의 잔액이 충분한지 확인
-        require(balanceOf[msg.sender] >= _value);
-        //user의 잔액을 빼고
-        balanceOf[msg.sender] -= _value;
-        //station의 잔액에 더함.
-        balanceOf[_toStation] += _value;
-        emit Transfer(msg.sender, _toStation, _value);
-        return true;
+    //수동채굴 (owner가 발급하도록)
+    function manualMine(address _toUser, uint _value) public payable onlyOwner {
+        transfer(_toUser, _value);
     }
 
-    //추가로 있으면 좋을 것 같은 기능
-    //- token추가로 생성(owner만 가능하게)
+    //게임비 유저로부터 지하철 역에 전달
+    function sendGameCost(address _toStation, uint256 _value) public payable {
+        transfer(_toStation, _value);
+    }
 }
