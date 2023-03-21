8a3099bccc1b3ade1bc388073fc064c46de7cfa7
wonseokjee <wsji9404@gmail.com>
2023-03-21 10:47:07 +0900

    Feat: WonToken.sol 생성, myToken 파일 삭제

---
+++ BC/contracts/WonToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Won {
+    string public name = "Won Token";
+    string public symbol = "Won";
+    uint256 public totalSupply = 10000000000000000000000000000; // 1 million tokens 100억개
+    uint8 public decimals = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public payable returns (bool success) {
+        // require that the value is greater or equal for transfer
+        require(balanceOf[msg.sender] >= _value);
+        // transfer the amount and subtract the balance
+        balanceOf[msg.sender] -= _value;
+        // add the balance
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    //추가로 있으면 좋을 것 같은 기능
+    //- token추가로 생성(owner만 가능하게)
+}
