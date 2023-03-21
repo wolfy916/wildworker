cda018d86ea89f7ad4f75b121723ac48850377b1
wonseokjee <wsji9404@gmail.com>
2023-03-21 16:48:54 +0900

    Refactor: totalSupply 100억won으로 변경

--- BC/contracts/WonToken.sol
+++ BC/contracts/WonToken.sol
@@ -5,7 +5,7 @@ contract WonToken {
     string public name = "Won Token";
     string public symbol = "Won";
     // uint256 public totalSupply = 10000000000000000000000000000; // 1 million tokens 100억개
-    uint256 public totalSupply = 100000000000; // 1 million tokens 100억개
+    uint256 public totalSupply = 10000000000; // 1 million tokens 100억개
     uint8 public decimals = 0;
     address owner;
 
@@ -13,9 +13,9 @@ contract WonToken {
 
     mapping(address => uint256) public balanceOf;
 
-    //밑줄 문자를 입력하면 함수가 계속되게 됨 위 제어자를 실행한 다음 함수로 이어지라는 것. 
+    //밑줄 문자를 입력하면 함수가 계속되게 됨 위 제어자를 실행한 다음 함수로 이어지라는 것.
     //조건에 부합하지 않는다면 함수를 계속하지 않아서 접근하는 것을 막음.
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner, "onlyOwner can call this function");
         _;
     }
@@ -25,10 +25,10 @@ contract WonToken {
         owner = msg.sender;
     }
 
-    function transfer(address _to, uint256 _value)
-        public payable
-        returns (bool success)
-    {
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public payable returns (bool success) {
         // require that the value is greater or equal for transfer
         require(balanceOf[msg.sender] >= _value);
         // transfer the amount and subtract the balance
@@ -40,16 +40,12 @@ contract WonToken {
     }
 
     //수동채굴 (owner가 발급하도록)
-    function manualMine(address _toUser, uint _value) public payable onlyOwner{
+    function manualMine(address _toUser, uint _value) public payable onlyOwner {
         transfer(_toUser, _value);
     }
 
-
     //게임비 유저로부터 지하철 역에 전달
-        function sendGameCost(address _toStation, uint256 _value)
-        public payable{
-            transfer(_toStation, _value);
+    function sendGameCost(address _toStation, uint256 _value) public payable {
+        transfer(_toStation, _value);
     }
-
-
 }
