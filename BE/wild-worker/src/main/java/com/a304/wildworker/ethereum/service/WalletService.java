6575c08b360276e4055b437dcc6db75451ddd9b6
오민성 <qww1552@naver.com>
2023-03-22 10:55:43 +0900

    Feat: 지갑 생성 시 초기 가스비 입금

--- BE/wild-worker/src/main/java/com/a304/wildworker/ethereum/service/WalletService.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/ethereum/service/WalletService.java
@@ -3,11 +3,7 @@ package com.a304.wildworker.ethereum.service;
 import com.a304.wildworker.ethereum.exception.InvalidAddressException;
 import java.io.File;
 import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import org.springframework.stereotype.Service;
-import org.web3j.crypto.CipherException;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
@@ -17,20 +13,29 @@ import org.web3j.protocol.core.methods.response.EthGetBalance;
 public class WalletService {
 
     private final Web3j web3j;
+    private final RootWallet rootWallet;
 
-    public WalletService(Web3j web3j) {
+    public WalletService(Web3j web3j, RootWallet rootWallet) {
         this.web3j = web3j;
+        this.rootWallet = rootWallet;
     }
 
     public String createWorkerWallet(
             String walletPassword) // walletPassword를 통해 내부적으로 privateKey를 생성해서 사용
-            throws InvalidAlgorithmParameterException, CipherException, NoSuchAlgorithmException, IOException, NoSuchProviderException {
-
+            throws Exception {
         File defaultDirectory = new File(WalletUtils.getDefaultKeyDirectory());
+        String walletFile = WalletUtils.generateLightNewWalletFile(walletPassword,
+                defaultDirectory);
 
-        return WalletUtils.generateLightNewWalletFile(walletPassword, defaultDirectory);
+        String address = WalletUtils.loadCredentials(walletPassword, walletFile)
+                .getAddress();
+
+        rootWallet.sendGasTo(address); // 초기 가스비 입금
+
+        return walletFile;
     }
 
+    // TODO: 2023-03-22 won을 읽어와야 함
     public EthGetBalance getEthBalance(String address) throws IOException {
         if (!WalletUtils.isValidAddress(address)) {
             throw new InvalidAddressException();
