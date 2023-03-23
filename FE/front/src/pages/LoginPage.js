80d5ebfaf2868e0640f0b714a5428144b48291b0
wonseokjee <wsji9404@gmail.com>
2023-03-23 13:05:44 +0900

    Feat: 로그인 연결 시도 실패

--- FE/front/src/pages/LoginPage.js
+++ FE/front/src/pages/LoginPage.js
@@ -2,14 +2,25 @@ import * as React from "react";
 import "./LoginPage.css";
 import loginTitle from "../asset/image/intro_title.png";
 import loginKakao from "../asset/image/kakao_login.png";
+import http from "../api/Http.js";
 import { useNavigate } from "react-router";
 
 function LoginPage() {
   const navigate = useNavigate();
   function loginClickHandler() {
     // 로그인 버튼 클릭시 이벤트
+    http
+      .get("/auth/login", {
+        headers: {
+          "Access-Control-Allow-Origin": "https://j8a304.p.ssafy.io/api",
+        },
+        withCredentials: true,
+      })
+      .then(res => {
+        console.log(res);
+      });
     console.log("kakao login btn clicked!");
-    navigate("/main");
+    // navigate("/main");
   }
   return (
     <div className="login-background">
