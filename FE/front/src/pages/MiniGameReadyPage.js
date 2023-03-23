e8d454bf6930585b7581c768b3e0b1ff6784c820
wonseokjee <wsji9404@gmail.com>
2023-03-23 12:34:48 +0900

    Feat: pvp 페이지 이동 flow, router 설정. gameReadypage

---
+++ FE/front/src/pages/MiniGameReadyPage.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./MiniGameReadyPage.css";
+
+function MainPage() {
+  const [timeLeft, setTimeLeft] = useState(2);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    if (timeLeft === 0) {
+      navigate("/pvp/minigame");
+    }
+  }, [timeLeft, navigate]);
+  return (
+    <Box className="Pvp-Result" sx={{ position: "relative" }}>
+      <div className="minigame-ready">
+        <div className="minigame-ready-header">
+          <p>회식비 정산하기!!!</p>
+        </div>
+        <div className="minigame-ready-main-text">
+          <p>어제 과음한 탓에...</p>
+          <p>내 카드로 회식비를 긁었다...</p>
+          <p>정신차리고 회식비를 정산해보자!</p>
+        </div>
+
+        <div>
+          <p className="minigame-ready-timer">{timeLeft}</p>
+        </div>
+        <div className="minigame-ready-timer-text">
+          <p>초 후 시작...</p>
+        </div>
+        <div className="minigame-ready-gif"></div>
+      </div>
+    </Box>
+  );
+}
+
+export default MainPage;
