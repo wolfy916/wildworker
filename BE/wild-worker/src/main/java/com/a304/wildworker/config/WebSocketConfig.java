bde65d04bcec1dc9830da5e570ffc09e8caf178f
Seyoung Goo <0917sy@gmail.com>
2023-03-23 17:35:41 +0900

    feat: spring session websocket에 적용

--- BE/wild-worker/src/main/java/com/a304/wildworker/config/WebSocketConfig.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/config/WebSocketConfig.java
@@ -4,25 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.session.Session;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<Session> {
 
     @Value("${allowed-origins}")
     private final String[] allowedOrigins;
+    private final HttpHandshakeInterceptor interceptor;
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .addInterceptors(interceptor)
                 .setAllowedOrigins(allowedOrigins)
                 .withSockJS();
         registry.addEndpoint("/ws")
+                .addInterceptors(interceptor)
                 .setAllowedOrigins(allowedOrigins);
         registry.addEndpoint("/secured/ws")
                 .setAllowedOrigins(allowedOrigins);
