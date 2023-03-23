ee1a41759b95b9247542566ddc29a68d12b25992
Seyoung Goo <0917sy@gmail.com>
2023-03-23 13:53:42 +0900

    feat: fix login redirect uri

--- BE/wild-worker/src/main/java/com/a304/wildworker/config/service/CustomLoginSuccessHandler.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/config/service/CustomLoginSuccessHandler.java
@@ -3,12 +3,12 @@ package com.a304.wildworker.config.service;
 
 import com.a304.wildworker.common.Constants;
 import java.io.IOException;
-import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -18,21 +18,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    private final String clientUrl;
+
+    public CustomLoginSuccessHandler(@Value("${url.client}") String clientUrl) {
+        this.clientUrl = clientUrl;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws ServletException, IOException {
         log.info("login handler");
-        String referer = request.getHeader("Referer");
-        log.info("-- referer: {}", referer);
         HttpSession session = request.getSession();
-        String prevPage = (String) session
-                .getAttribute(Constants.SESSION_NAME_PREV_PAGE);
-        log.info("-- prevPage: {}", prevPage);
-        String redirectUrl = Optional.ofNullable(referer)
-                .orElse(Optional.ofNullable(prevPage).orElseThrow());
-        redirectUrl += "main";     //TODO. get from config?
         response.setHeader(Constants.SET_COOKIE,
                 generateCookie(Constants.JSESSIONID, session.getId()).toString());
+        String redirectUrl = clientUrl + "/main";
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 
