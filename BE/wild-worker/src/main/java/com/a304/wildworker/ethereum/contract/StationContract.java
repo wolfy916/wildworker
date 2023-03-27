be932bcc6ab570d62186913f1696d6b94705872b
오민성 <qww1552@naver.com>
2023-03-27 09:16:33 +0900

    Feat: 컨트랙트 비동기 요청

--- BE/wild-worker/src/main/java/com/a304/wildworker/ethereum/contract/StationContract.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/ethereum/contract/StationContract.java
@@ -4,13 +4,13 @@ import com.a304.wildworker.ethereum.helper.TransactionSendHelper;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,16 +19,13 @@ public class StationContract {
 
     private final TransactionSendHelper transactionSendHelper;
 
-    public void autoMine(String stationAddress, String userAddress, long amount)
+    public CompletableFuture<Void> autoMine(String stationAddress, String userAddress, long amount)
             throws IOException {
         Function function = new Function("autoMine",
                 List.of(new Address(userAddress), new Uint256(amount)),
                 Collections.emptyList());
 
-        TransactionReceipt receipt = transactionSendHelper.sendContract(stationAddress,
-                function);
-
-        log.info("autoMine : {}", receipt);
-        log.info("revert : {}", receipt.getRevertReason());
+        return transactionSendHelper.sendContractAsync(stationAddress, function)
+                .thenAccept(receipt -> log.info("autoMine : {}", receipt));
     }
 }
