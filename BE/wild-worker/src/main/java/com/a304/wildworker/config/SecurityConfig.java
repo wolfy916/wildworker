a4e45c4e218cdffea9ea0168f8002309a5cec7bc
Seyoung Goo <0917sy@gmail.com>
2023-03-29 11:02:56 +0900

    feat: allowedHeaders 추가

--- BE/wild-worker/src/main/java/com/a304/wildworker/config/SecurityConfig.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig<S extends Session> {
                     cors.setAllowedMethods(
                             List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     cors.setAllowedHeaders(
-                            List.of("DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type"));
+                            List.of("DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Cookie"));
                     cors.setAllowCredentials(true);
                     return cors;
                 })
