123e908f161c4388808324164c20981ac2e0bd27
변지윤 <bhi991228@naver.com>
2023-03-30 16:59:01 +0900

    Feat: 생성자 어노테이션 변경

--- BE/wild-worker/src/main/java/com/a304/wildworker/dto/request/InvestmentRequest.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/dto/request/InvestmentRequest.java
@@ -1,14 +1,14 @@
 package com.a304.wildworker.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
 @Getter
 @ToString
-@AllArgsConstructor
+@NoArgsConstructor
 public class InvestmentRequest {
 
     private Long investment;
