c10b0ebc3c3574252499f602ba37cea4ff54dba9
Seyoung Goo <0917sy@gmail.com>
2023-03-30 13:19:32 +0900

    feat: Match 생성

--- BE/wild-worker/src/main/java/com/a304/wildworker/domain/match/Match.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/domain/match/Match.java
@@ -3,34 +3,18 @@ package com.a304.wildworker.domain.match;
 import com.a304.wildworker.domain.common.MiniGameType;
 import com.a304.wildworker.domain.user.User;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@RequiredArgsConstructor
-public class Match {
+@AllArgsConstructor
+public abstract class Match {
 
-    public static final int timeLimitSec = 10;
-    private static final int defaultCost = 20;
-    private static final int defaultRunCost = 5;
-    private final List<User> users;
-    private final int league;
-    private final MiniGameType miniGameType;
+    protected final List<User> users;
+    protected MiniGameType miniGameType;
 
-    public Match(User user1, User user2, int league) {
-        users = List.of(user1, user2);
-        this.league = league;
-        miniGameType = null; //TODO: create rand
-    }
+    public abstract int getCost();
 
-    public int getCost() {
-        return defaultCost * (int) (Math.pow(10, league));
-    }
-
-    public int getRunCost() {
-        return defaultRunCost * (int) (Math.pow(10, league));
-    }
+    public abstract int getRunCost();
 
 }
