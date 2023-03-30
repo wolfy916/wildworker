0eab4547a6ada52bcf8a645bc191673f2ac26af9
변지윤 <bhi991228@naver.com>
2023-03-30 16:57:56 +0900

    Feat: Exception 처리 제외 (controllerAdvice 사용)

--- BE/wild-worker/src/main/java/com/a304/wildworker/controller/rest/InvestmentController.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/controller/rest/InvestmentController.java
@@ -5,20 +5,19 @@ import com.a304.wildworker.domain.sessionuser.SessionUser;
 import com.a304.wildworker.dto.request.InvestmentRequest;
 import com.a304.wildworker.exception.NotLoginException;
 import com.a304.wildworker.service.InvestService;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.crypto.CipherException;
 
 @Slf4j
 @RestController
@@ -30,26 +29,16 @@ public class InvestmentController {
 
     /* 역 투자 */
     @PostMapping("/{station-id}")
-    public ResponseEntity<?> investToStation(HttpServletRequest request,
+    public ResponseEntity<Void> investToStation(HttpServletRequest request,
             @PathVariable("station-id") Long stationId,
-            @RequestBody InvestmentRequest investmentRequest) {
+            @RequestBody InvestmentRequest investmentRequest) throws CipherException, IOException {
         HttpSession httpSession = request.getSession();
         SessionUser user = (SessionUser) Optional.of(
                         httpSession.getAttribute(Constants.SESSION_NAME_USER))
                 .orElseThrow(NotLoginException::new);
 
-        Map resultmap = new HashMap<>();
-        HttpStatus status;
+        investService.investToStation(stationId, user.getId(), investmentRequest.getInvestment());
 
-        try {
-            investService.investToStation(stationId, user.getId(),
-                    investmentRequest.getInvestment());
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            resultmap.put("message", e.getMessage());
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-
-        return new ResponseEntity<Map>(resultmap, status);
+        return ResponseEntity.ok().build();
     }
 }
