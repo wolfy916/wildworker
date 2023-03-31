ac4fde405c2d41db8d1de32cdd20e58f48c6895e
변지윤 <bhi991228@naver.com>
2023-03-31 09:36:44 +0900

    Feat: SystemData의 타임라인 시간 변수 수정

--- BE/wild-worker/src/main/java/com/a304/wildworker/domain/system/SystemData.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/domain/system/SystemData.java
@@ -13,7 +13,8 @@ import org.springframework.stereotype.Component;
 public class SystemData {
 
     // 타임라인 시간
-    private LocalDateTime prevBaseTime; // 현재 타임라인의 시작시간
+    private LocalDateTime prevBaseTime; // 이전 타임라인의 시작시간
+    private LocalDateTime nowBaseTime; // 현재 타임라인의 시작시간
     private LocalDateTime nextBaseTime; // 다음 타임라인의 시작시간
     private LocalDateTime autoMiningBaseTime;   // 자동 채굴 상태 초기화된 시간
 
@@ -22,9 +23,10 @@ public class SystemData {
         LocalTime nowTime = LocalTime.now();
 
         // 타임라인 시간 세팅
-        prevBaseTime = LocalDateTime.of(nowDate,
+        nowBaseTime = LocalDateTime.of(nowDate,
                 LocalTime.of(nowTime.getHour(), (nowTime.getMinute() / 10) * 10, 0));
-        nextBaseTime = prevBaseTime.plusMinutes(Constants.INTERVAL);
+        prevBaseTime = nowBaseTime.minusMinutes(Constants.INTERVAL);
+        nextBaseTime = nowBaseTime.plusMinutes(Constants.INTERVAL);
 
         // 자동 채굴 초기화 시간 세팅
         LocalDate autoMineDate = (nowTime.getHour() < 3) ? nowDate.minusDays(1) : nowDate;
@@ -32,18 +34,23 @@ public class SystemData {
         autoMiningBaseTime = LocalDateTime.of(autoMineDate, LocalTime.of(autoMineTime, 0, 0));
     }
 
-    public String getPrevBaseTime() {
+    public String getPrevBaseTimeString() {
         return prevBaseTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
-    public String getNextBaseTime() {
+    public String getNowBaseTimeString() {
+        return nowBaseTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+
+    public String getNextBaseTimeString() {
         return nextBaseTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
-    
+
     /* 타임라인 시간 갱신 */
     public void updateBaseTime() {
-        prevBaseTime = nextBaseTime;
-        nextBaseTime = prevBaseTime.plusMinutes(Constants.INTERVAL);
+        prevBaseTime = nowBaseTime;
+        nowBaseTime = nextBaseTime;
+        nextBaseTime = nextBaseTime.plusMinutes(Constants.INTERVAL);
     }
 
     /* 자동 채굴 초기화 시간 갱신 */
