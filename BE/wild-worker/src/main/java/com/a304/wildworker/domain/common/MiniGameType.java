2674068a5a2fb7e6df608456230c72a5e64d4ef5
Seyoung Goo <0917sy@gmail.com>
2023-04-02 16:05:37 +0900

    feat: MiniGameType enum, Match class 추가

--- BE/wild-worker/src/main/java/com/a304/wildworker/domain/common/MiniGameType.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/domain/common/MiniGameType.java
@@ -1,5 +1,19 @@
 package com.a304.wildworker.domain.common;
 
+import java.util.List;
+import java.util.Random;
+
 public enum MiniGameType {
+    CALCULATE_GAME,         //계산 게임
+    CLICKER_GAME,           //두더지잡기
+    ROCK_PAPER_SCISSORS;    //가위바위보
+
+    private static final List<MiniGameType> VALUES = List.of(values());
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static MiniGameType random() {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
 
 }
