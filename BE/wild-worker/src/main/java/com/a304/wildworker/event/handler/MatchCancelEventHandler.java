66e106686a43e034fb0177b1ebe39b994d1ba0ed
Seyoung Goo <0917sy@gmail.com>
2023-04-04 00:44:00 +0900

    feat: add MatchCancelEvent

--- BE/wild-worker/src/main/java/com/a304/wildworker/event/handler/MatchCancelEventHandler.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/event/handler/MatchCancelEventHandler.java
@@ -4,7 +4,8 @@ import com.a304.wildworker.domain.match.Match;
 import com.a304.wildworker.dto.response.MatchCancelResponse;
 import com.a304.wildworker.dto.response.common.MiniGameType;
 import com.a304.wildworker.dto.response.common.WSBaseResponse;
-import com.a304.wildworker.event.MatchCancelEvent;
+import com.a304.wildworker.event.minigame.MatchCancelEvent;
+import com.a304.wildworker.service.ActiveUserService;
 import com.a304.wildworker.service.MessageService;
 import com.a304.wildworker.service.MiniGameLogService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MatchCancelEventHandler {
 
+    private final ActiveUserService activeUserService;
     private final MessageService messageService;
     private final MiniGameLogService miniGameLogService;
 
@@ -24,11 +26,12 @@ public class MatchCancelEventHandler {
     public void sendMessage(MatchCancelEvent event) {
         log.info("event occur: MatchCancelEvent - send message: {}", event);
         Match match = event.getMatch();
-        match.getUsers().forEach(activeUser -> {
-            String sessionId = activeUser.getWebsocketSessionId();
-            WSBaseResponse<MatchCancelResponse> response = WSBaseResponse
-                    .miniGame(MiniGameType.CANCEL)
-                    .data(MatchCancelResponse.of(match.isRunner(activeUser.getUserId())));
+        match.getUsers().forEach(user -> {
+            String sessionId = activeUserService.getSessionIdById(user.getId());
+            WSBaseResponse<MatchCancelResponse> response =
+                    WSBaseResponse
+                            .miniGame(MiniGameType.CANCEL)
+                            .data(MatchCancelResponse.of(match.isRunner(user.getId())));
             messageService.sendToUser(sessionId, response);
         });
     }
