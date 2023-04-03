7e4b2d82e1880e977a8c4c681d9789c123352b33
변지윤 <bhi991228@naver.com>
2023-04-03 13:20:17 +0900

    Feat: 칭호 관련 상수 추가

--- BE/wild-worker/src/main/java/com/a304/wildworker/common/Constants.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/common/Constants.java
@@ -25,4 +25,20 @@ public final class Constants {
     public static final long AMOUNT_MANUAL_MINE = 100L; //수동 채굴 금액
     public static final long AMOUNT_AUTO_MINE = 100L;   //자동 채굴 금액
 
+    // 칭호 아이디
+    public static final Long RISK_TAKER = 0L;
+    public static final Long RUNNER = 1L;
+    public static final Long POOR = 2L;
+    public static final Long RICH = 3L;
+    public static final Long LOOSER = 4L;
+    public static final Long WINNER = 5L;
+
+    // 칭호 조건
+    public static final int CONDITION_RISK_TAKER = 20;
+    public static final int CONDITION_RUNNER = 20;
+    public static final int CONDITION_POOR = 0;
+    public static final int CONDITION_RICH = 100000;
+    public static final int CONDITION_LOOSER = 10;
+    public static final int CONDITION_WINNER = 10;
+
 }
