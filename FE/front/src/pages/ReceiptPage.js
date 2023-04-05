4d7da358e3955809fa769249f4f6323347f631de
wonseokjee <wsji9404@gmail.com>
2023-04-05 17:59:10 +0900

    Fix: setStore 위치 변경

--- FE/front/src/pages/ReceiptPage.js
+++ FE/front/src/pages/ReceiptPage.js
@@ -1,43 +1,44 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ReceiptPage.css";
 
 function ReceiptPage(props) {
-  const gameResultData = props.gameResultData;
+  // const gameResultData = props.gameResultData;
   const setStore = props.setStore;
-  console.log(gameResultData);
-  // const gameResultData = {
-  //   isWinner: true,
-  //   enemy: {
-  //     name: "권태형",
-  //     title: "신도림의 지배자",
-  //     characterType: 1,
-  //   },
-  //   result: {
-  //     me: 12,
-  //     enemy: 11,
-  //   },
-  //   receipt: {
-  //     cost: -20,
-  //     runCost: 0,
-  //     reward: 40,
-  //     commission: -2,
-  //     total: 18,
-  //   },
-  // };
+  // console.log(gameResultData);
+  const gameResultData = {
+    isWinner: true,
+    enemy: {
+      name: "권태형",
+      title: "신도림의 지배자",
+      characterType: 1,
+    },
+    result: {
+      me: 12,
+      enemy: 11,
+    },
+    receipt: {
+      cost: -20,
+      runCost: 0,
+      reward: 40,
+      commission: -2,
+      total: 18,
+    },
+  };
 
-  setStore((prev) => {
-    // console.log(prev);
-    // console.log(prev.gameResult);
-    return {
-      ...prev,
-      gameResult: null,
-      gameStart: null,
-    };
-  });
   const navigate = useNavigate();
   function handleTouchStart(event) {
     // 다음 페이지로 이동하는 로직을 작성합니다.
+    setStore((prev) => {
+      // console.log(prev);
+      // console.log(prev.gameResult);
+      return {
+        ...prev,
+        gameResult: null,
+        gameStart: null,
+      };
+    });
+    console.log(gameResultData);
     navigate("/main");
     console.log("go to mainPage");
   }
