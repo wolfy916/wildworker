f28b9eca59b9308b0f918c15007349ac3faed655
wonseokjee <wsji9404@gmail.com>
2023-04-06 10:10:18 +0900

    feat: Winner에 무승부 분기 추가

--- FE/front/src/pages/ResultPage.js
+++ FE/front/src/pages/ResultPage.js
@@ -36,7 +36,7 @@ function ResultPage(props) {
   // };
 
   // const gameResultData = {
-  //   winner: false,
+  //   winner: 0,
   //   enemy: {
   //     name: "권태형",
   //     title: "신도림의 지배자",
@@ -54,8 +54,6 @@ function ResultPage(props) {
   //     total: 18,
   //   },
   // };
-
-  // const winner = gameResultData.winner;
 
   const navigate = useNavigate();
 
@@ -77,8 +75,9 @@ function ResultPage(props) {
           {/* 승리 패배시 컴포넌트 분리 */}
           {gameResultData ? (
             {
-              true: <BattleCharWinner characterType={userData.characterType} />,
-              false: <BattleCharLoser characterType={userData.characterType} />,
+              0: <BattleCharType characterType={userData.characterType} />,
+              1: <BattleCharWinner characterType={userData.characterType} />,
+              2: <BattleCharLoser characterType={userData.characterType} />,
             }[gameResultData.winner]
           ) : (
             <BattleCharType characterType={userData.characterType} />
@@ -87,11 +86,12 @@ function ResultPage(props) {
         {/* 승리했을때 패배했을때 분리하기 */}
         {gameResultData ? (
           {
-            true: <p className="battle-result-win-p2">승리</p>,
-            false: <p className="battle-result-lose-p2">패배</p>,
+            0: <p className="battle-result-draw-p2">무승부</p>,
+            1: <p className="battle-result-win-p2">승리</p>,
+            2: <p className="battle-result-lose-p2">패배</p>,
           }[gameResultData.winner]
         ) : (
-          <p className="battle-result-win-p2">대기</p>
+          <p className="battle-result-draw-p2">대기</p>
         )}
 
         <div className="battle-result-p3">
@@ -109,13 +109,18 @@ function ResultPage(props) {
           {/* 승리 패배시 분리 */}
           {gameResultData ? (
             {
-              false: (
+              2: (
                 <BattleCharWinner
                   characterType={matchingData.enemy.characterType}
                 />
               ),
-              true: (
+              1: (
                 <BattleCharLoser
+                  characterType={matchingData.enemy.characterType}
+                />
+              ),
+              0: (
+                <BattleCharType
                   characterType={matchingData.enemy.characterType}
                 />
               ),
@@ -128,11 +133,12 @@ function ResultPage(props) {
         {/* 승리 패배시 분리 */}
         {gameResultData ? (
           {
-            false: <p className="battle-result2-win-p2">승리</p>,
-            true: <p className="battle-result2-lose-p2">패배</p>,
+            2: <p className="battle-result2-win-p2">승리</p>,
+            1: <p className="battle-result2-lose-p2">패배</p>,
+            0: <p className="battle-result2-draw-p2">무승부</p>,
           }[gameResultData.winner]
         ) : (
-          <p className="battle-result2-lose-p2">대기</p>
+          <p className="battle-result2-draw-p2">대기</p>
         )}
 
         <div className="battle-result2-p3">
@@ -151,8 +157,9 @@ function ResultPage(props) {
         {/* 승리했을때 패배했을때 컴포넌트로 분리하기 */}
         {gameResultData ? (
           {
-            true: <p className="battleResult-talk-p">결투에서 승리했다!!!!</p>,
-            false: <p className="battleResult-talk-p">결투에서 패배했다...</p>,
+            1: <p className="battleResult-talk-p">결투에서 승리했다!!!!</p>,
+            2: <p className="battleResult-talk-p">결투에서 패배했다...</p>,
+            0: <p className="battleResult-talk-p">무승부다...</p>,
           }[gameResultData.winner]
         ) : (
           <p className="battleResult-talk-p">...</p>
