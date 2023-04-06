76d0ad452647197397c9b6eac727bb1ed05d10d7
DESKTOP-DOGVPUB\SSAFY <bhi991228@naver.com>
2023-04-06 14:32:08 +0900

    Fix: 로직 변경

--- BE/wild-worker/src/main/java/com/a304/wildworker/dto/response/InvestmentRankResponse.java
+++ BE/wild-worker/src/main/java/com/a304/wildworker/dto/response/InvestmentRankResponse.java
@@ -3,8 +3,10 @@ package com.a304.wildworker.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Setter
 @Getter
 @ToString
 @Builder
